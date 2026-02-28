1079d0131b927b37c1a499d1ed5089b160e38af1
mou <mou.reitetsu@gmail.com>
2026-02-28 18:05:47 +0900

    init and start db in docker

---
+++ apps/backend/prisma/seed.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import { generateKeyBetween } from 'fractional-indexing';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    
+}
